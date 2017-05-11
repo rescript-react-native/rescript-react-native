e3dd61282c367986589946f14a50269e0e3fafcf
Sascha Timme <sascha.timme@gmail.com>
2017-05-11 16:05:04 +0200

    Change index.android.js to match ios

--- RNTester/index.android.js
+++ RNTester/index.android.js
@@ -4,50 +4,10 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import { comp as App } from "../lib/js/RNTester/src/rNTesterApp.js";
+import React from "react";
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+  AppRegistry
 } from 'react-native';
 
-export default class RNTester extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
-AppRegistry.registerComponent('RNTester', () => RNTester);
+AppRegistry.registerComponent('RNTester', () => App);
