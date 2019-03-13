04ed33fec885fce3375144be06b21758c305f89a
Maxime Thirouin <git@moox.io>
2019-03-13 09:35:24 +0100

    Add logo to website
    Closes #325

--- docs/scripts/build-docs.sh
+++ docs/scripts/build-docs.sh
@@ -56,3 +56,13 @@ echo "<< Adding static public files..."
 cp -r ${PUBLIC} ${DOCS}
 echo ">> Done!"
 
+echo "<< Editing html files to inject meta..."
+META="<link rel=\"apple-touch-icon\" sizes=\"180x180\" href=\"/apple-touch-icon.png\">
+<link rel=\"icon\" type=\"image/png\" sizes=\"32x32\" href=\"/favicon-32x32.png\">
+<link rel=\"icon\" type=\"image/png\" sizes=\"16x16\" href=\"/favicon-16x16.png\">
+<link rel=\"manifest\" href=\"/site.webmanifest\">
+<link rel=\"mask-icon\" href=\"/safari-pinned-tab.svg\" color=\"#222222\">
+<meta name=\"msapplication-TileColor\" content=\"#222222\">
+<meta name=\"theme-color\" content=\"#ffffff\">"
+yarn replace "<html xmlns=\"http://www.w3.org/1999/xhtml\"><head><title>" "<html xmlns=\"http://www.w3.org/1999/xhtml\"><head>$META<title>" ${DOCS} --recursive --include="*.html"
+echo ">> Done!"
