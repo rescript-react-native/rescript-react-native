aabd9340f93639a8e73ffa7347a4c688d9cbb61b
Christoph Knittel <christoph.knittel@cca.io>
2019-04-15 16:12:44 +0200

    Remove PromiseUtils in the spirit of zero cost.

--- bs-react-native-next/src/apis/Linking.bs.js
+++ bs-react-native-next/src/apis/Linking.bs.js
@@ -1,11 +1,1 @@
-'use strict';
-
-var ReactNative = require("react-native");
-var PromiseUtils$ReactNative = require("../utils/PromiseUtils.bs.js");
-
-function getInitialURL(param) {
-  return ReactNative.Linking.getInitialURL().then(PromiseUtils$ReactNative.nullableToOption);
-}
-
-exports.getInitialURL = getInitialURL;
-/* react-native Not a pure module */
+/* This output is empty. Its source's type definitions, externals and/or unused code got optimized away. */
--- bs-react-native-next/src/utils/PromiseUtils.re
+++
@@ -1,2 +0,0 @@
-let nullableToOption = stringOrNull =>
-  Js.Promise.resolve(stringOrNull->Js.Null.toOption);
