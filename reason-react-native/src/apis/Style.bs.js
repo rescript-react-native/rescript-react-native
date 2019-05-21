4157638a223f14d29022a863aa1134894d1805c9
MoOx <git@moox.io>
2019-05-21 19:42:50 +0200

    Style: move Transform method into Style for convenience
    I deleted all the style docs as I am working on it & don't want to keep it in sync ftm.
    
    Closes #503

--- reason-react-native/src/apis/Style.bs.js
+++ reason-react-native/src/apis/Style.bs.js
@@ -5,5 +5,15 @@ function pct(num) {
   return num.toString() + "%";
 }
 
+function deg(num) {
+  return num.toString() + "deg";
+}
+
+function rad(num) {
+  return num.toString() + "rad";
+}
+
 exports.pct = pct;
+exports.deg = deg;
+exports.rad = rad;
 /* No side effect */
