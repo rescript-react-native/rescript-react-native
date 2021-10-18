b963ea034b6967717d8447147dca8270451ce757
Maxime Thirouin <git@moox.io>
2021-10-18 09:53:14 +0200

    Update NativeEventEmitter.bs.js

--- src/apis/NativeEventEmitter.bs.js
+++ src/apis/NativeEventEmitter.bs.js
@@ -1,7 +1,1 @@
-'use strict';
-
-
-var Subscription = {};
-
-exports.Subscription = Subscription;
-/* No side effect */
+/* This output is empty. Its source's type definitions, externals and/or unused code got optimized away. */
